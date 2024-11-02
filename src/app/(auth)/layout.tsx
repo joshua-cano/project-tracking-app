3c75bc7fc8e3f9cdb372da4a25095dd1bb793720
Joshua Cano <joshuacano@unknownbe0b31d5e2c1.attlocal.net>
2024-11-02 12:34:11 -0500

    Add height to nav

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -18,7 +18,7 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
-        <nav className="flex justify-between items-center">
+        <nav className="flex justify-between items-center h-[73px]">
           <Link href="/">
             <Image src={logo} alt="logo" />
           </Link>
