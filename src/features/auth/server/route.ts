20bdaf4d31ba22680df8d90e8bf96734a1e51240
Joshua Cano <joshuacano@unknownbe0b31d5e2c1.attlocal.net>
2024-11-02 00:52:01 -0500

    Set cookie same site to none

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -26,7 +26,7 @@ const app = new Hono()
       path: "/",
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 60 * 60 * 24 * 30,
     });
 
@@ -45,7 +45,7 @@ const app = new Hono()
       path: "/",
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 60 * 60 * 24 * 30,
     });
 
