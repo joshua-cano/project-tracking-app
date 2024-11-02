76eb0323681be2481e18a293ba46f79fe79e7a85
Joshua Cano <joshuacano@unknownbe0b31d5e2c1.attlocal.net>
2024-11-02 01:19:26 -0500

    Test user prod

--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,13 +1,14 @@
 // import { redirect } from "next/navigation";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
-// import { getCurrent } from "@/features/auth/queries";
+import { getCurrent } from "@/features/auth/queries";
 
 const WorkspaceCreatePage = async () => {
-  // const user = await getCurrent();
+  const user = await getCurrent();
   // if (!user) redirect("/sign-in");
 
   return (
     <div className="w-full lg:max-w-xl">
+      {JSON.stringify(user)}
       <CreateWorkspaceForm />
     </div>
   );
